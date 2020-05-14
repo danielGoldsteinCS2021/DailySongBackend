1f727bcfcaac24dfd58b9c02f16ba7f889d81f07
Daniel Goldstein <18djg@queensu.ca>
2020-05-14 16:06:57 -0400

    intial commit

---
+++ src/main/java/com/example/DailySong/DailySongApplication.java
@@ -0,0 +1,13 @@
+package com.example.DailySong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DailySong/DailySongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DailySong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
