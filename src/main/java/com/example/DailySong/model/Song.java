76e628dfd49827f0f2c21e9db2552a9293222614
Daniel Goldstein <18djg@queensu.ca>
2020-05-15 12:02:13 -0400

    added constructor for model and json property tags - posts should hopefully work now

--- src/main/java/com/example/DailySong/model/Song.java
+++ src/main/java/com/example/DailySong/model/Song.java
@@ -1,14 +1,23 @@
 package com.example.DailySong.model;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.UUID;
 
 @Document(collection = "DailySong")
 public class Song {
-    private String title;
-    private String artist;
-    private String year;
-    private String web_url;
+    private final String title;
+    private final String artist;
+    private final String year;
+    private final String web_url;
+
+    public Song(@JsonProperty("title") String title, @JsonProperty("artist") String artist,
+                @JsonProperty("year") String year, @JsonProperty("web_url") String web_url){
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        this.web_url = web_url;
+    }
 
     /*
         Didn't create any setters yet just getters - plan is to only be able
