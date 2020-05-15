0ebc499f0fdfec23c2d193b041f06627fc5f104d
Daniel Goldstein <18djg@queensu.ca>
2020-05-15 12:55:02 -0400

    post requests set up and return 200 ok value from postman

--- src/main/java/com/example/DailySong/api/SongController.java
+++ src/main/java/com/example/DailySong/api/SongController.java
@@ -4,8 +4,11 @@ import com.example.DailySong.model.Song;
 import com.example.DailySong.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("api/v1/song")
 @RestController
 public class SongController {
     private final SongService songService;
@@ -15,7 +18,7 @@ public class SongController {
         this.songService = songService;
     }
     @PostMapping
-    public void addSong(Song song){
+    public void addSong(@RequestBody Song song){
         songService.addSong(song);
     }
 }
